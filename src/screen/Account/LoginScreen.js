1d418c97231b3f9d3cc31abccbdd30d8cf542407
choyeongwoo <choyeongwoo.s@gmail.com>
2022-07-07 15:47:19 +0900

    LoginScreen 수정

--- src/screen/Account/LoginScreen.js
+++ src/screen/Account/LoginScreen.js
@@ -1,16 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
-  Text,
+  // Text,
   TextInput,
   View,
   Dimensions,
-  Button,
+  // Button,
   Alert,
   PermissionsAndroid,
 } from 'react-native';
 import axios from 'axios'
 import { login } from '../../axios';
+import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider } from "native-base";
 
 export default function LoginScreen(props) {
   const [inputId, setInputId] = useState("")
@@ -50,66 +51,30 @@ export default function LoginScreen(props) {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.helloText}>로그인중인네이티브</Text>
-        <TextInput
-                id="id-address"
-                name="id"
-                type="text"
-                placeholder="id"
-                // onKeyDown={(e) => { console.log(e); }}
-                onChange={(e)=>{setInputId(e.nativeEvent.text)}}
-              />
-        <TextInput
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Password"
-                secureTextEntry = {true}
-                // onKeyDown={(e) => {console.log(e) }}
-                onChange={(e)=>{setInputPw(e.nativeEvent.text)}}
-              />
-        <Button
-          color="#ff1178"
-          title="Login"
-          onPress={()=> {login({id: inputId, pw: inputPw}), props.navigation.navigate('TodoList')}}
-          />
-      </View>
-    </View>
+    <NativeBaseProvider>
+      <Center w="100%" marginTop={20}>
+        <Box safeArea p="2" py="8" w="90%" maxW="290">
+          <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+            color: "warmGray.50"
+          }}>
+            POSCO ICT - 5
+          </Heading>
+          <Heading mt="1" _dark={{
+            color: "warmGray.200"
+          }} color="coolGray.600" fontWeight="medium" size="xs">
+            물류, 창고 재고 모니터링 시스템
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl.Label>ID</FormControl.Label>
+            <Input onChange={(e) => { setInputId(e.nativeEvent.text) }} />
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" onChange={(e) => { setInputPw(e.nativeEvent.text) }} />
+            <Button mt="2" onPress={() => { login({ id: inputId, pw: inputPw }), props.navigation.navigate('TodoList') }}>
+              Sign in
+            </Button>
+          </VStack>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-  const screenWidth = Dimensions.get('screen').width;
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    //  backgroundColor: '#1f1f1f',
-      alignItems: 'center',
-      paddingTop: 250,
-    },
-    helloText: {
-    //  color: 'white',
-      marginBottom: 20,
-      fontSize: 30,
-    },
-    textInput: {
-      padding: 5,
-      paddingStart: 15,
-      backgroundColor: '#3b3b3b',
-      width: screenWidth * 0.8,
-      borderRadius: 25,
-      marginBottom: 15,
-    //  color: 'white',
-      fontWeight: '600',
-    },
-    loginBtn: {
-      paddingHorizontal: 25,
-      paddingVertical: 10,
-      backgroundColor: '#ff1178',
-      borderRadius: 25,
-     // color: 'black',
-      textAlign: 'center',
-    },
-  });
---
+++ src/screen/NavigationBar/CustomDrawerContent.js
@@ -0,0 +1,154 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+} from '@react-navigation/drawer';
+const CustomDrawerContent = (props) => {
+    return (
+        <DrawerContentScrollView {...props} style={{ backgroundColor: '#354259' }}>
+            <View style={styles.header}>
+                <View style={styles.headerContainer}>
+                    <Text style={styles.headerTitle}>메뉴</Text>
+                    {/* <Icon name="MehOutlined" size={30} /> */}
+                </View>
+            </View>
+
+            <View style={styles.buttonWrap}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('TodoList');
+                            props.setTitle('Main');
+                        }}>
+                        <Text style={styles.title}>Main</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('Inventory');
+                            props.setTitle('Inventory');
+                        }}>
+                        <Text style={styles.title}>Inventory</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('Import');
+                            props.setTitle('Import');
+                        }}>
+                        <Text style={styles.title}>Import</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('Export');
+                            props.setTitle('Export');
+                        }}>
+                        <Text style={styles.title}>Export</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('Move');
+                            props.setTitle('Move');
+                        }}>
+                        <Text style={styles.title}>Move</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate('TodoList');
+                            props.setTitle('TodoList');
+                        }}>
+                        <Text style={styles.title}>TodoList</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </DrawerContentScrollView>
+    );
+}
+const styles = StyleSheet.create({
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+
+    buttonContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+
+        // backgroundColor: '#f0f0f0',
+        // borderColor: '#dccab2',
+        // borderWidth: 3,
+        // marginBottom: 20,
+        padding: 20,
+    },
+    icon: {
+        marginBottom: 10,
+    },
+    title: {
+        fontSize: 15,
+        // color: '#243A73',
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+
+    header: {
+        // borderBottomWidth: '1px',
+        borderColor: '#D3D3D3',
+        // padding: 20,
+        marginTop: 20,
+        marginBottom: 40,
+    },
+
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // justifyContent: 'center',
+        paddingBottom: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#fff',
+    },
+
+    headerIcon: {
+        marginLeft: 20,
+    },
+
+    headerTitle: {
+        fontSize: 15,
+        color: '#fff',
+        textAlign: 'center',
+        marginLeft: 20,
+    },
+
+    footer: {
+        paddingTop: 20,
+        borderTopWidth: 1,
+        borderTopColor: '#fff',
+        // backgroundColor: '#fff',
+        marginTop: 50,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+
+    footerTitle: {
+        fontSize: 15,
+        color: '#fff',
+        marginRight: 20,
+        marginLeft: 10,
+    },
+});
+
+export default CustomDrawerContent
