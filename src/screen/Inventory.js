ea0feb71d3e61018095ec5c8ec9938d5fc534fa1
wjddkswl97 <wjddkswl97@gmail.com>
2022-07-08 11:44:16 +0900

    inventory 페이지

--- src/screen/Inventory.js
+++ src/screen/Inventory.js
@@ -1,81 +1,113 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Button} from '@react-native-material/core';
+//import {Button} from '@react-native-material/core';
 import {ListItem} from '@react-native-material/core';
 import {View, StyleSheet, ScrollView, StatusBar} from 'react-native';
-import { Box, Heading, AspectRatio, Image, Text, Center, HStack, Stack, NativeBaseProvider } from "native-base";
+import {
+  Box,
+  Heading,
+  AspectRatio,
+  Image,
+  Text,
+  Center,
+  HStack,
+  Stack,
+  NativeBaseProvider,
+  Modal,
+  VStack,
+  Button,
+} from 'native-base';
 
 import Navbar from './Navbar';
+import InventoryModal from './InventoryModal';
+
 //창고안에 있는 재고들 출력
 //최근순 , 상태 순 setstate 에 담
 export default function Inventory(props) {
   const [inventoryList, setInventoryList] = useState([]);
- 
+  const [showModal, setShowModal] = useState(false);
   //axios
   useEffect(() => {
-    axios.defaults.baseURL = "http://13.230.73.69:8080/inventory";
+    axios.defaults.baseURL = 'http://13.230.73.69:8080/inventory';
     axios
-    //.get(`/warehouse/${warehouseCode}`)
-    .get(`/warehouse/GA04`)
-      .then((res) => {
-       // console.log(res.data, "받아온 데이터");
+      //.get(`/warehouse/${warehouseCode}`)
+      .get(`/warehouse/GA04`)
+      .then(res => {
+       // console.log(res.data, '받아온 데이터');
         setInventoryList(res.data);
         //setInventoryList([...res.data]);
-        
-    })
-    .catch((err) => {console.log(err); });
-  }, [])
-//console.log(inventoryList);
-  // inventoryList.map((inventoryList) => {
-    
-  // });
-  const title = " 제목";
-  const subTitle = "부제목";
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const title = ' 제목';
+  const subTitle = '부제목';
   return (
     <NativeBaseProvider>
-    <Box alignItems="center" marginY={3}>
-            <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700"
-            }} _web={{
-                shadow: 2,
-                borderWidth: 0
-            }} _light={{
-                backgroundColor: "gray.50"
-            }}>
+      <ScrollView>
+        {inventoryList.map(inventoryItem => {
+          return (
+            <Box alignItems="center" marginY={3}>
+              <Box
+                onPress={() => setShowModal(true)}
+                width="80"
+                rounded="lg"
+                overflow="hidden"
+                borderColor="coolGray.200"
+                borderWidth="1"
+                _dark={{
+                  borderColor: 'coolGray.600',
+                  backgroundColor: 'gray.700',
+                }}
+                _web={{
+                  shadow: 2,
+                  borderWidth: 0,
+                }}
+                _light={{
+                  backgroundColor: 'gray.50',
+                }}>
                 <Stack p="4" space={3}>
-                    <Stack space={2}>
-                        <Heading size="md" ml="-1">
-                            {title}
-                        </Heading>
-                        <Text fontSize="xs" _light={{
-                            color: "violet.500"
-                        }} _dark={{
-                            color: "violet.400"
-                        }} fontWeight="500" ml="-0.5" mt="-1">
-                            {subTitle}
-                        </Text>
-            </Stack>
-            
-                    <Text fontWeight="400">
-                        Bengaluru (also called Bangalore) is the center of India's high-tech
-              industry. The city is also known for its parks and nightlife.
-              {
-                inventoryList.map((inventoryItem) => {
+                  <Stack space={2}>
+                    <Heading size="sm" ml="-1">
+                    lot_no : ####
+                    </Heading>
+                    <Text
+                      fontSize="md"
+                      _light={{
+                        color: 'violet.500',
+                      }}
+                      _dark={{
+                        color: 'violet.400',
+                      }}
+                      fontWeight="500"
+                      ml="-0.5"
+                      mt="-1">
+                      amount : {inventoryItem.amount}
+                    </Text>
+                  </Stack>
                  
-                    {inventoryItem.amount}
-                 
-                   //console.log(inventoryItem.amount);
-                }
-              )
-              }
-            </Text>
-            
+                  <Text fontWeight="400">
+                    item_name : {inventoryItem.item_name}
+                  </Text>
+                  <Text fontWeight="400">
+                    item_code : {inventoryItem.item_code}
+                  </Text>
+                  <Text fontWeight="400">
+                    inventory_date : {inventoryItem.inventory_date}
+                  </Text>
                 </Stack>
+                <InventoryModal
+                  showModal={showModal}
+                  setShowModal={setShowModal}
+                  inventoryItem={inventoryItem}
+                />
+              </Box>
             </Box>
-      </Box>
-      </NativeBaseProvider>
+          );
+        })}
+      </ScrollView>
+    </NativeBaseProvider>
   );
 }
-
-
---
+++ src/screen/InventoryModal.js
@@ -0,0 +1,105 @@
+import {View} from 'react-native';
+import React from 'react';
+import {
+  Box,
+  Heading,
+  AspectRatio,
+  Image,
+  Text,
+  Center,
+  HStack,
+  Stack,
+  NativeBaseProvider,
+  Modal,
+  VStack,
+  Button,
+} from 'native-base';
+const InventoryModal = props => {
+  return (
+    <View>
+      <Button
+        colorScheme="violet"
+        onPress={() => {
+          props.setShowModal(true);
+        }}>
+        자세히보기
+      </Button>
+      <Modal
+        isOpen={props.showModal}
+        onClose={() => props.setShowModal(false)}
+        size="lg">
+        <Modal.Content maxWidth="350">
+          <Modal.CloseButton />
+          <Modal.Header>Order</Modal.Header>
+          <Modal.Body>
+            <VStack space={3}>
+            <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">customer</Text>
+                <Text color="blueGray.400">{props.inventoryItem.customer}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">item_code</Text>
+                <Text color="blueGray.400">{props.inventoryItem.item_code}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">inventory_date</Text>
+                <Text color="blueGray.400">{props.inventoryItem.inventory_date}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">height</Text>
+                <Text color="blueGray.400">{props.inventoryItem.height}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">industry_family</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.industry_family}
+                </Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">location</Text>
+                <Text color="blueGray.400">{props.inventoryItem.location}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">industry_family</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.industry_family}
+                </Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">product_family</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.product_family}
+                </Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">state</Text>
+                <Text color="blueGray.400">{props.inventoryItem.state}</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">status_cause</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.status_cause}
+                </Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">stock_quality_status</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.stock_quality_status}
+                </Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+                <Text fontWeight="medium">stock_type</Text>
+                <Text color="blueGray.400">
+                  {props.inventoryItem.stock_type}
+                </Text>
+              </HStack>
+            </VStack>
+          </Modal.Body>
+          <Modal.Footer></Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </View>
+  );
+};
+
+export default InventoryModal;
