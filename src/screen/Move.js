3c4b1994488f89e3eab3fc9f63238b6608bc8a3f
wjddkswl97 <wjddkswl97@gmail.com>
2022-07-08 17:57:57 +0900

    무브 탭 완성

--- src/screen/Move.js
+++ src/screen/Move.js
@@ -1,94 +1,51 @@
 import React, {useState} from 'react';
-import axios from 'axios';
-import {Button} from '@react-native-material/core';
-import {ListItem} from '@react-native-material/core';
-import {View, StyleSheet, ScrollView, StatusBar, Alert} from 'react-native';
-import Navbar from './Navbar';
+import {View, StyleSheet, Dimensions, StatusBar} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
+import Move_first from './Move_first';
 
-export default function Move(props) {
-  const [moveList, setMoveList] = useState([]);
+export default function TabViewExample() {
+  const FirstRoute = () => (
+    // <View style={[styles.scene, { backgroundColor: '#ffffff' }]}/>
+<Move_first/>
+  );
 
-  const onGetBarcodeMove = (barcodeValue, cmdType) => {
-    console.log('barcode value: ', barcodeValue);
-    //아래 함수의 파라미터로 문자열만 넘길 수 있음. barcodeValue가 문자열처럼 보이지만 문자열이 아닌 듯. String()는 작동하지 않음. JSON.stringify()는 작동함
-    //  Alert.alert("barcode value: ", barcodeValue);
-    if (cmdType == 'move') {
-      axios.defaults.baseURL = 'http://35.77.44.58:8080';
-      axios
-        .get('/move/lotno/' + barcodeValue)
-        // axios.get("http://35.77.20.236:8080/import/lotno/" +barcodeValue)
-        .then(res => {
-          console.log(123123123);
-          console.log(res.data);
-          console.log(res.data.instruction_no);
-          // axios.put("http://35.77.20.236:8080/import/import/" + res.data.instruction_no)
-          axios
-            .put('/move/move/' + res.data.instruction_no)
-            .then(res2 => {
-              Alert.alert('입고 완료되었습니다.');
-            })
-            .catch(e => {
-              console.log(e);
-              Alert.alert(e);
-            });
-        })
-        .catch(e => {
-          console.log(e);
-          Alert.alert(e);
-        });
-    }
-  };
+  const SecondRoute = () => (
+    <View style={[styles.scene, {backgroundColor: '#673ab7'}]} />
+  );
+  const finish = () => (
+    <View style={[styles.scene, {backgroundColor: '#FFFFFF'}]} />
+  );
+  const initialLayout = {width: Dimensions.get('window').width};
 
-  const keyList = [
-    ['상태', 'status'],
-    ['이름', 'item_name'],
-    ['수량', 'amount'],
-    ['일자', 'order_date'],
-  ];
-  const MakeSecondText = ListItem => {
-    let result = '';
-    for (let i = 0; i < 4; i++) {
-      result += keyList[i][0] + ': ' + ListItem[keyList[i][1]] + '\n';
-    }
-    return result;
-  };
+  const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+    3: finish,
+  });
+
+  const [index, setIndex] = useState(0);
+  const [routes, setRoutes] = useState([
+    {key: 'first', title: '이동예정'},
+    {key: 'second', title: '이동중'},
+    {key: '3', title: '이동완료'},
+  ]);
 
   return (
-    <View>
-      <Button
-        color="#f1a178"
-        title="이동 물품 확인"
-        onPress={() =>
-          props.navigation.navigate('BarcodeScanner', {
-            onGetBarcode: onGetBarcodeMove,
-            cmdType: 'move',
-          })
-        }
-      />
-      <ScrollView style={styles.scrollView}>
-        {moveList.map((value, index) => {
-          return (
-            <ListItem
-              title={value.lot_no}
-              secondaryText={MakeSecondText(value)}
-              key={index}
-            />
-          );
-        })}
-      </ScrollView>
-    </View>
+    <TabView
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      style={styles.container}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
+    marginTop: StatusBar.currentHeight,
   },
-  scrollView: {
-    backgroundColor: 'white',
-    marginTop: 0,
-    marginBottom: 50,
-    marginHorizontal: 70,
+  scene: {
+    flex: 1,
   },
 });
