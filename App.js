0ae7206653c2d090371ebd8338240915846b77ee
wjddkswl97 <wjddkswl97@gmail.com>
2022-07-11 10:55:12 +0900

    주석

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const App = () => {
     <NativeBaseProvider>
       <NavigationContainer>
         <MainNavigator />
-        <StackNavigator />
+        {/* <StackNavigator /> */}
       </NavigationContainer>
     </NativeBaseProvider>
   )
