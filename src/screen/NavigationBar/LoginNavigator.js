dccf62b50c52f5907f785baaeed776f44e47a0d6
choyeongwoo <choyeongwoo.s@gmail.com>
2022-07-12 09:36:28 +0900

    승엽꺼 병합완료

--- src/screen/NavigationBar/LoginNavigator.js
+++ src/screen/NavigationBar/LoginNavigator.js
@@ -1,15 +1,21 @@
-function LoginNavigator(){
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../Account/LoginScreen';
+
+function LoginNavigator() {
+    const StackApp = createStackNavigator();
     const navOptionHandler = () => ({
-      headerShown: false,
+        headerShown: false,
     });
-    return(
-      <StackApp.Navigator initialRouteName="LoginScreen">
-        <StackApp.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          options={navOptionHandler}
-        />
-      </StackApp.Navigator>
+    return (
+        <StackApp.Navigator initialRouteName="LoginScreen">
+            <StackApp.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                options={navOptionHandler}
+            />
+        </StackApp.Navigator>
     )
-  }
-  
+}
+
+export default LoginNavigator
