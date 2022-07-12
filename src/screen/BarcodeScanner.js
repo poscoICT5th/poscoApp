7dfe0b142568e00164e15efc0a019ea57f1220b1
yeop <yeopribo26@gmail.com>
2022-07-12 18:09:58 +0900

    toast

--- src/screen/BarcodeScanner.js
+++ src/screen/BarcodeScanner.js
@@ -1,62 +1,43 @@
-import React, {Component} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import {View, PermissionsAndroid, Platform, Text} from 'react-native';
 import {CameraScreen} from 'react-native-camera-kit';
 
-var isFirstGet = true;
-//default는 App.js에서만 사용해야 하는 듯
-class BarcodeScanner extends Component {
-  cmdType = null;
-  constructor(props) {
-    console.log('BarcodeScanner');
-    super(props);
-    this.cmdType = props.route.params.cmdType;
-  }
-
-  componentDidMount() {
-    //isFirstGet의 이전 값이 남아 있어서 다시 실행할 때 true를 할당해야 함
+const BarcodeScanner = props => {
+  var isFirstGet = true;
+  useEffect(() => {
     isFirstGet = true;
-  }
+  }, []);
 
-  componentWillUnmount() {}
-
-  /**
-   * 바코드 스캔
-   */
-
-  onBarcodeScan(barcodeValue) {
+  function onBarcodeScan(barcodeValue) {
     console.log('onBarcodeScan');
     if (!isFirstGet) {
       return;
     }
     isFirstGet = false;
     if (
-      this.cmdType == 'inventory' ||
-      this.cmdType == 'import' ||
-      this.cmdType == 'export' ||
-      this.cmdType == 'move'
+      props.route.params.cmdType == 'inventory' ||
+      props.route.params.cmdType == 'import' ||
+      props.route.params.cmdType == 'export' ||
+      props.route.params.cmdType == 'move'
     ) {
-      this.props.route.params.onGetBarcode(barcodeValue, this.cmdType);
+      props.route.params.onGetBarcode(barcodeValue, props.route.params.cmdType);
     } else {
-      this.props.route.params.onGetBarcode(barcodeValue, this.cmdType);
+      props.route.params.onGetBarcode(barcodeValue, props.route.params.cmdType);
     }
     //TODO 필요한 부분 구현하세요
     // this.props.navigation.pop();
 
     //called after te successful scanning of QRCode/Barcode
     console.log('scanned barcode value: ' + barcodeValue);
-
     setTimeout(() => {
       isFirstGet = true;
     }, 2000);
   }
 
-  //AAAAAZZZZZ
 
-  //TODO Home.js로 이동시키세요
-  checkCameraPermission() {}
-
-  render() {
-    return (
+  function checkCameraPermission() {}
+  return (
+    <View>
       <CameraScreen
         showFrame={true}
         //Show/hide scan frame
@@ -70,11 +51,11 @@ class BarcodeScanner extends Component {
         //Scanner Frame color
         onReadCode={event => {
           console.log(999);
-          return this.onBarcodeScan(event.nativeEvent.codeStringValue);
+          return onBarcodeScan(event.nativeEvent.codeStringValue);
         }}
       />
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default BarcodeScanner;
--- src/screen/Import.js
+++ src/screen/Import.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import axios from 'axios';
 import {Button} from '@react-native-material/core';
 import {ListItem} from '@react-native-material/core';
@@ -6,21 +6,22 @@ import {View, StyleSheet, ScrollView, StatusBar, Alert} from 'react-native';
 import useRootData from '../hooks/useRootData';
 import jwtDecode from 'jwt-decode';
 import Toast from 'react-native-easy-toast';
-
+import {  useToast } from "native-base";
 export default function Import(props) {
+  const toast = useToast();
   const [importList, setImportList] = useState([]);
   // store에서 token 갖고옴
-  const {token} = useRootData(
-    ({screenModeStore}) => ({
-      token: screenModeStore.token,
-    }),
-  );
+  const {token} = useRootData(({screenModeStore}) => ({
+    token: screenModeStore.token,
+  }));
 
   let team = jwtDecode(token.get().token).info.team;
   console.log(team);
 
   const toastRef = useRef();
-
+  const showCopyToast = useCallback(() => {
+    toastRef.current.show('주소가 복사되었습니다.');
+  }, []);
   const onGetBarcodeImport = (barcodeValue, cmdType) => {
     console.log('barcode value: ', barcodeValue);
     //아래 함수의 파라미터로 문자열만 넘길 수 있음. barcodeValue가 문자열처럼 보이지만 문자열이 아닌 듯. String()는 작동하지 않음. JSON.stringify()는 작동함
@@ -34,28 +35,33 @@ export default function Import(props) {
           console.log(123123123);
           console.log(res.data);
           console.log(res.data.instruction_no);
-          console.log(token2);
+          // console.log(token2);
           // setImportList(res.data);
           // axios.put("http://35.77.20.236:8080/import/import/" + res.data.instruction_no)
           axios
             .put('/import/import/' + res.data.instruction_no)
             .then(res2 => {
-              Alert.alert(res.data.instruction_no + ' 입고 완료되었습니다.');
+              // Alert.alert(res.data.instruction_no + ' 입고 완료되었습니다.');
+              toast.show({
+                title: res.data.instruction_no + ' 입고 완료되었습니다.',
+                placement: "bottom"
+              })
               axios
                 .get('/import/search?to_warehouse=' + team)
                 // .get('/import/search?to_warehouse=399')
                 .then(res3 => {
-                  console.log("창고코드 들어왔나연~");
+                  console.log('창고코드 들어왔나연~');
+                  setImportList(res3.data)
                 })
                 .catch(e => {
                   console.log(e);
                   Alert.alert(e);
-                })
+                });
             })
             .catch(e => {
               console.log(e);
               Alert.alert(e);
-            }); 
+            });
         })
         .catch(e => {
           console.log(e);
@@ -77,7 +83,13 @@ export default function Import(props) {
     }
     return result;
   };
-
+  const [click, setClick] = useState(false);
+  function trueClick() {
+    setClick(true);
+  }
+  function falseClick(params) {
+    setClick(false);
+  }
   return (
     <View>
       <Button
@@ -87,10 +99,11 @@ export default function Import(props) {
           props.navigation.navigate('BarcodeScanner', {
             onGetBarcode: onGetBarcodeImport,
             cmdType: 'import',
+            trueClick: trueClick,
+            click: click,
           })
         }
       />
-
       <ScrollView style={styles.scrollView}>
         {importList.map((value, index) => {
           return (
